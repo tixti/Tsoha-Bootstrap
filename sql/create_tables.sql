9e93cc8f3e7f42432e490076b332104b190ef26c
tilli <tilli@ubuntu.(none)>
2015-11-03 15:04:15 +0200

    CREATE TABLE ja DROP TABLE

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,24 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Käyttäjä(
+	ktunnus SERIAL PRIMARY KEY,
+	käyttäjätunnus varchar(50) NOT NULL,
+	salasana varchar(50) NOT NULL
+);
+
+CREATE TABLE Askare(
+	atunnus SERIAL PRIMARY KEY,
+	ktunnus INTEGER REFERENCES Käyttäjä(ktunnus),
+	nimi varchar(50) NOT NULL,
+	tärkeysaste INTEGER DEFAULT 1,
+	deadline DATE
+);
+
+CREATE TABLE Luokka(
+	ltunnus SERIAL PRIMARY KEY,
+	nimi varchar(50) NOT NULL
+);
+
+CREATE TABLE Askareenluokka(
+	altunnus SERIAL PRIMARY KEY,
+	atunnus INTEGER REFERENCES Askare(atunnus),
+	ltunnus INTEGER REFERENCES Luokka(ltunnus)
+);
