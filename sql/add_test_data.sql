7141ca84590facdaa44d4b492020085e5164b6a3
tilli <tilli@ubuntu.(none)>
2015-11-03 15:15:53 +0200

    SQL test_date

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,5 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Käyttäjä (käyttäjätunnus, salasana) VALUES ('Väiski', 'Väiski42');
+INSERT INTO Käyttäjä (käyttäjätunnus, salasana) VALUES ('Repe', 'Sorsa');
+INSERT INTO Askare (ktunnus, nimi, tärkeysaste, deadline) VALUES (1, 'Tenttiin luku', 5, '20151212');
+INSERT INTO Luokka (nimi) VALUES ('Opiskelu');
+INSERT INTO Askareenluokka (atunnus, ltunnus) VALUES (1, 1);
